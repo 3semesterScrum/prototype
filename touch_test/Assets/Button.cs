deca90e4d35f67615bc0067d3a10e282ef87464b
claus jensen <clau3028@student.eadania.dk>
2016-11-02 13:39:47 +0100

    first push

---
+++ touch_test/Assets/Button.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public Color DefaultColor;
+    public Color SelectedColor;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void OnTouchDown()
+    {
+        mat.color = SelectedColor;
+    }
+    void OnTouchUp()
+    {
+        mat.color = DefaultColor;
+    }
+    void OnTouchStay()
+    {
+        mat.color = SelectedColor;
+    }
+    void OnTouchExit()
+    {
+        mat.color = DefaultColor;
+    }
+}
