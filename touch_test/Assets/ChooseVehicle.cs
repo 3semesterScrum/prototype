0cf46f41290a5d69fee932d24a5f60ed0fcb5ba8
claus jensen <clau3028@student.eadania.dk>
2016-11-07 13:57:49 +0100

    menu + vehicle choice

---
+++ touch_test/Assets/ChooseVehicle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChooseVehicle : MonoBehaviour {
+
+   // public Vehicle vehicle;
+    string derp;
+	// Use this for initialization
+	void Start () {
+         derp = gameObject.name;
+        Debug.Log(derp);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadScene()
+    {
+        Vehicle.vehicle = derp;
+      SceneManager.LoadScene("HospitalPrototype");
+     //  vehicle.vehicle = derp;
+    }
+}
