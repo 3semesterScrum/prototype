bf8f75249f8a8c599b4ba5b61f380d28c6c40ecb
DESKTOP-KHDRP1R\Frederik <ninjakongenfh@yahoo.com>
2016-11-10 10:50:11 +0100

    Added Pushbutton... again

---
+++ touch_test/Assets/ButtonPush.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ButtonPush : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private Image buttonImg;
+    // Use this for initialization
+    void Start()
+    {
+        buttonImg = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(buttonImg.rectTransform, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            buttonImg.color = Color.red;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (buttonImg.color == Color.red)
+        {
+            buttonImg.color = Color.white;
+        }
+    }
+}
