43fa499b332001e5a50e9441a535951f48bd5aba
NielsJustesen <niels449@hotmail.com>
2016-11-10 10:09:30 +0100

    Ny branch stuff

---
+++ touch_test/Assets/CubeMotor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMotor : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+    public float drag = 0.1f;
+    public float terminalRotationSpeed = 25.0f;
+    public Vector3 MoveVector { set; get; }
+    public VirtualJoystick joystick;
+    private CarController car;
+
+    private Rigidbody thisRigidbody;
+	// Use this for initialization
+	void Start () {
+        car = GetComponent<CarController>();
+        thisRigidbody = gameObject.AddComponent<Rigidbody>();
+        //thisRigidbody.maxAngularVelocity = terminalRotationSpeed;
+        //thisRigidbody.drag = drag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveVector = PoolInput();
+        if(car == null)
+        {
+            Move();
+        }
+        else
+        {
+            car.Move(MoveVector.x, MoveVector.z, MoveVector.z, 0f);
+        }
+	}
+
+    private void Move()
+    {
+        thisRigidbody.AddForce(MoveVector * moveSpeed);
+    }
+
+    private Vector3 PoolInput()
+    {
+        Vector3 dir = Vector3.zero;
+
+        dir.x = joystick.Horizontal();
+        dir.z = joystick.Vertical();
+        if (dir.magnitude>1)
+        {
+            dir.Normalize();
+        }
+        return dir;
+    }
+}
---
+++ touch_test/Assets/Vehicle.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Vehicle : MonoBehaviour
+{
+
+    public static string vehicle;
+    public GameObject Car;
+    public Canvas Touch;
+    // Use this for initialization
+
+
+    void Start()
+    {
+
+
+        DontDestroyOnLoad(this);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CreateVehicle() == false)
+        {
+            CreateVehicle();
+        }
+
+    }
+    private bool CreateVehicle()
+    {
+        if (!GameObject.FindGameObjectWithTag("vehicle"))
+        {
+
+            if (vehicle == "patient bus")
+            {
+
+                // farve.material.color = Color.blue;
+                //  GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                GameObject car = Instantiate(Car);
+                // var derp = GameObject.Find("BackgroundImage");
+                //   car.gameObject.GetComponent<CubeMotor>().joystick=(VirtualJoystick)derp;
+                car.name = "patient bus";
+                car.tag = "vehicle";
+                // Canvas  canvas = GameObject.Instantiate(Touch);
+                var derp = GameObject.FindGameObjectWithTag("GameController");
+                Debug.LogError(derp.GetComponentInChildren<Image>());
+                car.gameObject.GetComponent<CubeMotor>().joystick = derp.GetComponentInChildren<Image>().GetComponent<VirtualJoystick>();
+
+
+            }
+            if (vehicle == "fejeblad")
+            {
+                // farve.material.color = Color.green;
+                GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                capsule.name = "fejeblad";
+                capsule.tag = "vehicle";
+            }
+            if (vehicle == "el truck")
+            {
+                GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                cylinder.name = "el truck";
+                cylinder.tag = "vehicle";
+                //  farve.material.color = Color.red;
+            }
+            if (vehicle == "Senge transport")
+            {
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.name = "senge transport";
+                sphere.tag = "vehicle";
+                // farve.material.color = Color.yellow;
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
