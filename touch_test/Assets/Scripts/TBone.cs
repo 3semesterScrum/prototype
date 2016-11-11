acfc3c39ea33e9cd1beba8a889001da7fe902f87
NielsJustesen <niels449@hotmail.com>
2016-11-11 11:03:37 +0100

    Nyt hospital, WalkingNPC fra ny...

--- touch_test/Assets/Scripts/TBone.cs
+++ touch_test/Assets/Scripts/TBone.cs
@@ -7,19 +7,39 @@ public class TBone : MonoBehaviour
     private GameObject Car;
     [SerializeField]
     private GameObject playerVehicle;
-
-	void Start ()
-    {
-
-	}
-	
+    float minSpeed;
+    float maxSpeed = 1;
+    float actualSpeed;
+    bool canSpawn = true;
+    float ready = 0;
 	void Update ()
     {
-
+        if (!canSpawn)
+        {
+            ready += Time.deltaTime;
+            if (ready > 4)
+            {
+                canSpawn = true;
+                ready = 0;
+            }
+        }
 	}
 
     public void CreateTBoner()
     {
         GameObject tBoner = Instantiate(Car);
+        tBoner.transform.position = new Vector3(this.transform.position.x -6, this.transform.position.y + 1, this.transform.position.z + 7);
+        Debug.Log("T-Boner spawned");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        minSpeed = playerVehicle.GetComponent<CarController>().CurrentSpeed;
+        if (minSpeed > maxSpeed && canSpawn)
+        {
+            CreateTBoner();
+            canSpawn = false;
+        }
+        Debug.Log("The minSpeed was: " + minSpeed);
     }
 }
